b2d282aee93275403b18f7011145ee123e315ccf
ahmedrsaeed <ahmedrazasaeed@gmail.com>
2019-03-08 20:37:10 -0600

    Updating how the difference is computed

--- diff.js
+++ diff.js
@@ -23,66 +23,106 @@ var diff = function diff(left, right) {
       return currObject && _typeof(currObject) === "object" ? currObject[currKey] : {}[currKey];
     }, object);
   };
+  
+  var getKeysWithTypesAndVals = function getKeysWithTypesAndVals(object) {
+    return Object.keys(object).map(function(key) {
+      var val = object[key];
+      return {
+        key: key,
+        val: val,
+        type: Array.isArray(val) 
+          ? "array" 
+          : val === null 
+            ? "null" 
+            : _typeof(val)
+      };     
+    })
+  }
 
-  var linearizeJson = function linearizeJson(baseObject) {
+  var getKeysOnly = function getKeysOnly(keyTypeArray) {
+    return keyTypeArray.map(function(keyType){
+      return keyType.key;
+    });
+  }
+  
+  var linearizeJson = function linearizeJson(baseObject, identifier) {
     if (!(baseObject && _typeof(baseObject) === "object")) return [];
-    var keyStack = [Object.keys(baseObject)];
+    var keyStack = [getKeysWithTypesAndVals(baseObject)];
     var linearKey = [];
-    var linearlist = [];
+    var flatObject = {};
 
     while (keyStack.length > 0) {
+      
       linearKey.pop();
       var currentKeys = keyStack.pop();
 
       while (currentKeys && currentKeys.length > 0) {
         var currNode = currentKeys.pop();
         if (!currNode) continue;
-        linearKey.push(currNode);
-        var value = getSafely(linearKey, baseObject);
+        
+                
+        linearKey.push(currNode.key);     
+        var stringKey = JSON.stringify(linearKey.slice(1));
+        
+        flatObject[stringKey] = {
+          stringKey: stringKey,
+          key: linearKey.slice(),
+          [identifier]: {              
+            type: currNode.type
+          }
+        }
+            
 
-        if (value && _typeof(value) === "object") {
+        if (["object", "array"].indexOf(currNode.type) > -1) {
           keyStack.push(currentKeys);
-          currentKeys = Object.keys(value);
-        } else {
-          linearlist.push({
-            key: JSON.stringify(linearKey),
-            value: value
-          });
+          currentKeys = getKeysWithTypesAndVals(currNode.val);
+        } else {    
+          flatObject[stringKey][identifier].val = currNode.val
           linearKey.pop();
         }
       }
     }
 
-    return linearlist;
+    return flatObject;
   };
 
   var getDifferences = function getDifferences(left, right) {
-    var linearLeft = linearizeJson(left);
-    var linearRight = linearizeJson(right);
-    var allKeys = linearLeft.concat(linearRight).map(function (keyVal) {
-      return keyVal.key;
-    });
-    return allKeys.filter(function (key, index, arr) {
-      return arr.indexOf(key) === index;
-    }).filter(function (key, index, arr) {
-      return arr.findIndex(function (keyInternal) {
-        return key.startsWith(keyInternal.slice(0, -1)) ? keyInternal !== key : false;
-      }) < 0;
-    }).map(function (key) {
-      return {
-        stringVal: key,
-        parsed: JSON.parse(key)
-      };
-    }).map(function (key) {
-      return {
-        key: key.stringVal,
-        left: getSafely(key.parsed, left),
-        right: getSafely(key.parsed, right)
-      };
-    }).filter(function (diff) {
-      return diff.left !== diff.right;
-    });
+    var linearLeft = linearizeJson(left, "left");
+    var linearRight = linearizeJson(right, "right");
+    
+    for (var attrname in linearLeft) { 
+      if (linearRight[attrname]){
+        linearRight[attrname].left = linearLeft[attrname].left; 
+      }else{
+        linearRight[attrname] = linearLeft[attrname]
+      }
+    }
+    
+    
+    
+    return Object.keys(linearRight)
+      .map(function (key) {        
+        return linearRight[key];
+      })
+      .filter(function(key){
+        return !key.left || !key.right || key.left.type !== key.right.type || key.left.val !== key.right.val;
+      })
+      .filter(function (key, index, arr) {
+        return arr.findIndex(function (other) {
+          return key.stringKey.startsWith(other.stringKey.slice(0, -1)) ? other.stringKey !== key.stringKey : false;
+      }) < 0})
+      // })
+      .map(function (key) {
+        return {
+          key: key.stringKey,
+          left: (key.left || {}).val || getSafely(key.key, left),
+          right: (key.right || {}).val || getSafely(key.key, right),
+          leftMissing: !key.left,
+          rightMissing: !key.right,
+          typeDifferent: (key.left || {}).type !== (key.right || {}).type && ("left is " + (key.left || {}).type +" and right is "+(key.right || {}).type)
+        };
+      });
   };
 
-  return getDifferences(left, right);
+  return getDifferences({root: left}, {root: right});
 };
